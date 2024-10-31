3d4f8d50798bc5b90d35db1402b3ae7b986fda74
Kevin <kevindev@gmail.com>
2024-10-31 11:32:00 +0700

    - done add multi language

--- app/src/main/java/com/Meditation/Sounds/frequencies/models/Language.kt
+++ app/src/main/java/com/Meditation/Sounds/frequencies/models/Language.kt
@@ -3,4 +3,4 @@ package com.Meditation.Sounds.frequencies.models
 import com.Meditation.Sounds.frequencies.R
 import java.io.Serializable
 
-data class Language(val image: Int = R.drawable.ic_english_usa, val name: String,val translateName: String , val code: String) : Serializable
+data class Language(val image: Int = R.drawable.ic_english_usa, val name: String , val code: String) : Serializable
--- app/src/main/java/com/Meditation/Sounds/frequencies/utils/LanguageUtils.kt
+++ app/src/main/java/com/Meditation/Sounds/frequencies/utils/LanguageUtils.kt
@@ -16,91 +16,77 @@ class LanguageUtils {
             return listOf(
                 Language(
                     R.drawable.ic_united_kingdom,
-                    "English",
                     context.getString(R.string.lang_en),
                     "en"
                 ),
                 Language(
                     R.drawable.ic_spain,
-                    "Spanish",
                     context.getString(R.string.lang_es),
                     "es"
                 ),
                 Language(
                     R.drawable.ic_france,
-                    "French",
                     context.getString(R.string.lang_fr),
                     "fr"
                 ),
+//                Language(
+//                    R.drawable.ic_china,
+//                    "China",
+//                    context.getString(R.string.lang_zh),
+//                    "zh"
+//                ),
                 Language(
                     R.drawable.ic_china,
-                    "China",
-                    context.getString(R.string.lang_zh),
-                    "zh"
-                ),
-                Language(
-                    R.drawable.ic_china,
-                    "Japanese",
                     context.getString(R.string.lang_ja),
                     "ja"
                 ),
                 Language(
                     R.drawable.ic_china,
-                    "Vietnamese",
                     context.getString(R.string.lang_vi),
                     "vi"
                 ),
                 Language(
                     R.drawable.ic_china,
-                    "German",
                     context.getString(R.string.lang_de),
                     "de"
                 ),
                 Language(
                     R.drawable.ic_china,
-                    "Chinese - Traditional",
                     context.getString(R.string.lang_zh_hant),
                     "zh_hant"
                 ),
                 Language(
                     R.drawable.ic_china,
-                    "Chinese - Simplified",
                     context.getString(R.string.lang_zh_hans),
                     "zh_hans"
                 ),
                 Language(
                     R.drawable.ic_china,
-                    "Hindi",
                     context.getString(R.string.lang_hi),
                     "hi"
                 ),
                 Language(
                     R.drawable.ic_china,
-                    "Arabic",
                     context.getString(R.string.lang_ar),
                     "ar"
                 ),
                 Language(
                     R.drawable.ic_china,
-                    "Portuguese",
                     context.getString(R.string.lang_pt),
                     "pt"
                 ),
                 Language(
                     R.drawable.ic_china,
-                    "Korean",
                     context.getString(R.string.lang_ko),
                     "ko"
                 ),
                 Language(
                     R.drawable.ic_china,
-                    "Italian",
                     context.getString(R.string.lang_it),
                     "it"
                 ),
                 Language(
                     R.drawable.ic_china,
-                    "Bengali",
                     context.getString(R.string.lang_bn),
                     "bn"
                 ),
@@ -109,7 +95,7 @@ class LanguageUtils {
 
         fun getLanguage(languageCode: String, context: Context): Language {
             return getLanguages(context).firstOrNull { it.code == languageCode } ?: Language(
-                R.drawable.ic_united_kingdom, "English", context.getString(R.string.lang_en), "en"
+                R.drawable.ic_united_kingdom, context.getString(R.string.lang_en), "en"
             )
         }
 
