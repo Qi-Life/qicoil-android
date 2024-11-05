e61c090bce4808b5209cb70c23073554e62465e3
Kevin <kevindev@gmail.com>
2024-11-05 15:03:33 +0700

    - done mutl language

--- app/src/main/java/com/Meditation/Sounds/frequencies/utils/LanguageUtils.kt
+++ app/src/main/java/com/Meditation/Sounds/frequencies/utils/LanguageUtils.kt
@@ -36,57 +36,57 @@ class LanguageUtils {
 //                    "zh"
 //                ),
                 Language(
-                    R.drawable.ic_china,
+                    R.drawable.ic_japan,
                     context.getString(R.string.lang_ja),
                     "ja"
                 ),
                 Language(
-                    R.drawable.ic_china,
+                    R.drawable.ic_vietnam,
                     context.getString(R.string.lang_vi),
                     "vi"
                 ),
                 Language(
-                    R.drawable.ic_china,
+                    R.drawable.ic_german,
                     context.getString(R.string.lang_de),
                     "de"
                 ),
                 Language(
-                    R.drawable.ic_china,
+                    R.drawable.ic_china_tradi,
                     context.getString(R.string.lang_zh_hant),
-                    "zh_hant"
+                    "zh-rTW"
                 ),
                 Language(
-                    R.drawable.ic_china,
+                    R.drawable.ic_china_impli,
                     context.getString(R.string.lang_zh_hans),
-                    "zh_hans"
+                    "zh-rCN"
                 ),
                 Language(
-                    R.drawable.ic_china,
+                    R.drawable.ic_hindi,
                     context.getString(R.string.lang_hi),
                     "hi"
                 ),
                 Language(
-                    R.drawable.ic_china,
+                    R.drawable.ic_arabic_flag,
                     context.getString(R.string.lang_ar),
                     "ar"
                 ),
                 Language(
-                    R.drawable.ic_china,
+                    R.drawable.ic_portu,
                     context.getString(R.string.lang_pt),
                     "pt"
                 ),
                 Language(
-                    R.drawable.ic_china,
+                    R.drawable.ic_korea,
                     context.getString(R.string.lang_ko),
                     "ko"
                 ),
                 Language(
-                    R.drawable.ic_china,
+                    R.drawable.ic_italia,
                     context.getString(R.string.lang_it),
                     "it"
                 ),
                 Language(
-                    R.drawable.ic_china,
+                    R.drawable.ic_bengali,
                     context.getString(R.string.lang_bn),
                     "bn"
                 ),
